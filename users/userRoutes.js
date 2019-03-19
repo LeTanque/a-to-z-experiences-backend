8e126357b24809a5c8996494cf8d8b5e55b07029
matt0418 <mfeldma7@gmail.com>
2019-03-19 12:28:13 -0400

    added ability to get experiences from a provider and experiences a user is attending

--- users/userRoutes.js
+++ users/userRoutes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const Users = require('./UserHelpers')
 const restricted = require('../auth/middleware')
+const db = require('../data/dbConfig')
 
 
 router.get('/', restricted, async (req, res) => {
@@ -10,6 +11,37 @@ router.get('/', restricted, async (req, res) => {
     } catch(error) {
         console.log(error)
         res.status(500).json(error)
+    }
+})
+
+
+//Gets the user and the experiences they are providing
+router.get('/:id/provider', restricted, async (req, res) => {
+    const id = req.params.id
+    try {
+        const user = await Users.findById(id)
+        if (user) {
+            const experiences = await db('experiences').where({'provider_id': id})
+            user.experiences = experiences
+            return res.status(200).json(user)
+        } else {
+            return res.status(404).json({ message: 'There is no user with the provided id' })
+        }
+    } catch(error) {
+        console.log(error)
+        return res.status(500).json(error)
+    }
+})
+
+//gets the experiences that the user is attending
+router.get('/:id/experiences', restricted, async (req, res) => {
+    const id = req.params.id
+    try {
+        const experiences = await Users.findExperiences(id)
+        return res.status(200).json(experiences)
+    } catch(error) {
+        console.log(error)
+        return res.status(500).json(error)
     }
 })
 
